1e0930ec920c2d1e5a78bcc934d854101bfcf2c7
Eduard Parvu <eduard.parvu@rinftech.com>
2024-12-19 23:13:58 +0200

    shader config

--- src/main/java/org/example/book/game/GameLogic.java
+++ src/main/java/org/example/book/game/GameLogic.java
@@ -1,7 +1,9 @@
 package org.example.book.game;
 
+import org.eclipse.collections.api.factory.primitive.FloatLists;
 import org.example.book.engine.IAppLogic;
 import org.example.book.engine.Window;
+import org.example.book.engine.graph.Mesh;
 import org.example.book.engine.graph.Renderer;
 import org.example.book.engine.scene.Scene;
 
@@ -13,7 +15,14 @@ public final class GameLogic implements IAppLogic {
 
     @Override
     public void init(Window window, Scene scene, Renderer renderer) {
+        final var positions = FloatLists.immutable.of(
+                0.0f, 0.5f, 0.0f,
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f
+        );
 
+        final var mesh = Mesh.create(positions, 3);
+        scene.addMesh("cube", mesh);
     }
 
     @Override
